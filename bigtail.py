38a9e013232e618c4c5e109edc69ed5942d5d104
Mosky <mosky.tw@gmail.com>
2013-05-18 23:40:27 +0800

    the version which works well

---
+++ bigtail.py
@@ -0,0 +1,84 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+
+def tail(file_or_path, buffer_size=None):
+    
+    if buffer_size is None:
+        # `buffer_size` is not a constant.
+        buffer_size = 4096 # 4 kB
+
+    if isinstance(file_or_path, basestring):
+        f = open(file_or_path)
+    else:
+        f = file_or_path
+
+    # move to the end and prepare for first reading
+    f.seek(0, os.SEEK_END)
+    if f.tell()-buffer_size >= 0:
+        f.seek(-buffer_size, os.SEEK_END)
+    else:
+        buffer_size = f.tell()
+        f.seek(0)
+
+    end_pos = None
+
+    fragment = None
+
+    while True:
+
+        # read the chunk form file
+        chunk = f.read(buffer_size)
+        if not chunk:
+            if fragment:
+                yield fragment
+            break
+
+        # handle the chunk
+        lines = chunk.splitlines(True)
+        #print 'lines:', repr(lines)
+
+        if fragment:
+            if lines[-1].endswith('\n'):
+                yield fragment
+            else:
+                yield lines.pop(-1)+fragment
+            fragment = ''
+
+        for line in reversed(lines[1:]):
+            yield line
+
+        if lines:
+            fragment = lines[0]
+        #print 'fragment:', repr(fragment)
+
+        #print '---'
+        # prepare for next reading
+        step_size = -buffer_size*2 # include the range it read
+        if f.tell()+step_size >= 0:
+            f.seek(step_size, os.SEEK_CUR)
+        else:
+            # read the final chunk
+            buffer_size = f.tell()-buffer_size
+            f.seek(0)
+
+if __name__ == '__main__':
+
+    import sys
+
+    path = 'access.log'
+    if len(sys.argv) == 2:
+        path = sys.argv[1]
+
+    for line in tail(path):
+        print line,
+
+    #for i in range(10):
+    #    print 'i = ', i
+    #    for line in tail('ordered.list', i):
+    #        print line,
+    #    print
+
+    #for line in tail('ordered.list', 1):
+    #    print line,
