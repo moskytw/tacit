db6132e031c9550b75328fe0b18a167ed50b8dba
Mosky <mosky.tw@gmail.com>
2013-05-20 00:20:47 +0800

    make tail use tail_chunks

--- bigtail.py
+++ bigtail.py
@@ -44,33 +44,9 @@ def tail_chunks(file_or_path, buffer_size=None):
 
 def tail(file_or_path, buffer_size=None):
     
-    if buffer_size is None:
-        # `buffer_size` is not a constant.
-        buffer_size = 4096 # 4 kB
-
-    if isinstance(file_or_path, basestring):
-        f = open(file_or_path)
-    else:
-        f = file_or_path
-
-    # move to the end and prepare for first reading
-    f.seek(0, os.SEEK_END)
-    if f.tell()-buffer_size >= 0:
-        f.seek(-buffer_size, os.SEEK_END)
-    else:
-        buffer_size = f.tell()
-        f.seek(0)
-
     fragment = ''
 
-    while True:
-
-        # read the chunk form file
-        chunk = f.read(buffer_size)
-        if not chunk:
-            if fragment:
-                yield fragment
-            break
+    for chunk in tail_chunks(file_or_path, buffer_size):
 
         # parse the chunk
         lines = chunk.splitlines(True)
@@ -115,11 +91,5 @@ def tail(file_or_path, buffer_size=None):
             for line in reversed(lines[1:]):
                 yield line
 
-        # prepare for next reading
-        step_size = -buffer_size*2 # include the range it read
-        if f.tell()+step_size >= 0:
-            f.seek(step_size, os.SEEK_CUR)
-        else:
-            # read the final chunk
-            buffer_size = f.tell()-buffer_size
-            f.seek(0)
+    if fragment:
+        yield fragment
