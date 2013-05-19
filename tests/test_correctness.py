a4d470085dd5f40633ee893c46ededb79253bdeb
Mosky <mosky.tw@gmail.com>
2013-05-19 10:25:04 +0800

    make correctness test better

--- tests/test_correctness.py
+++ tests/test_correctness.py
@@ -1,12 +1,22 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import sys
 from bigtail import tail
 
+excepted_lines = ['9\n', '8\n', '7\n', '6\n', '5\n', '4\n', '3\n', '2\n', '1\n']
+
 for i in range(10):
+
     print 'i = ', i
-    for line in tail('data/ordered.list', i):
+
+    for excepted_line, line in zip(excepted_lines, tail('data/ordered.list', i)):
+
+        if excepted_line != line:
+            print >> sys.stderr, 'Warning: i=%d, except %r, but got %r' % (i, excepted_line, line)
+
         print line,
+    
     print
 
 #for line in tail('data/ordered.list', 1):
