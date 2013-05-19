a16470d80b4500e8f258abe6eac199cdc03ee6a4
Mosky <mosky.tw@gmail.com>
2013-05-20 00:39:18 +0800

    make correctness better

--- tests/test_correctness.py
+++ tests/test_correctness.py
@@ -3,22 +3,31 @@
 
 import sys
 from itertools import izip
+
 from bigtail import tail
 
-excepted_lines = ['9\n', '8\n', '7\n', '6\n', '5\n', '4\n', '3\n', '2\n', '1\n', '0.1\n', '0.01\n']
+ordered_list_path = 'data/ordered.list'
 
-for i in range(10):
+expected_lines = open(ordered_list_path).read().splitlines(True)
+expected_lines.reverse()
 
-    print 'i = ', i
+expected_count = len(expected_lines)
 
-    for excepted_line, line in izip(excepted_lines, tail('data/ordered.list', i)):
+for bsize in range(10):
 
-        if i > 0 and excepted_line != line:
-            print >> sys.stderr, 'Warning: i=%d, except %r, but got %r' % (i, excepted_line, line)
+    count = 0
 
-        print line,
-    
-    print
+    for expected_line, line in izip(
+        expected_lines,
+        tail(ordered_list_path, bsize)
+    ):
+        if line != expected_line:
+            print >> sys.stderr, 'error: bsize=%d, expected_line=%r, line=%r' % (bsize, expected_line, line)
+        count += 1
 
-#for line in tail('data/ordered.list', 1):
-#    print line,
+    if bsize > 0:
+        if count != expected_count:
+            print >> sys.stderr, 'error: bsize=%d, expected_count=%r, count=%r' % (bsize, expected_count, count)
+    else:
+        if count != 0:
+            print >> sys.stderr, 'error: bsize=%d, expected_count=0, count=%r' % (bsize, count)
