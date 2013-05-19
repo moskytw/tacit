7425c801ca5247cb961dd9564674139a76c4183c
Mosky <mosky.tw@gmail.com>
2013-05-19 20:28:41 +0800

    don't show None

--- tests/test_correctness.py
+++ tests/test_correctness.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 
 import sys
-from itertools import izip_longest
+from itertools import izip
 from bigtail import tail
 
 excepted_lines = ['9\n', '8\n', '7\n', '6\n', '5\n', '4\n', '3\n', '2\n', '1\n', '0.1\n', '0.01\n']
@@ -11,7 +11,7 @@ for i in range(10):
 
     print 'i = ', i
 
-    for excepted_line, line in izip_longest(excepted_lines, tail('data/ordered.list', i)):
+    for excepted_line, line in izip(excepted_lines, tail('data/ordered.list', i)):
 
         if i > 0 and excepted_line != line:
             print >> sys.stderr, 'Warning: i=%d, except %r, but got %r' % (i, excepted_line, line)
